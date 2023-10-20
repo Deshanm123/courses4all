0404925cad829d6a7cc620b22dbdfa102bc21071
Deshanm123 <deshanm123@gmail.com>
2023-10-20 19:11:48 +0100

    adding users by IdentityFramework

--- Courses4All/Startup.cs
+++ Courses4All/Startup.cs
@@ -31,7 +31,7 @@ namespace Courses4All
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
             services.AddDatabaseDeveloperPageExceptionFilter();
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddRazorPages();
         }
